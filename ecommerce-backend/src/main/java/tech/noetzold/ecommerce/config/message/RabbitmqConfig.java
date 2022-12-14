a26ef9e8e2a2c4b9253dd0da1f3bc42fcbd7e11b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-12-14 13:52:17 -0300

    Create RabbitmqConfig.java

---
+++ ecommerce-backend/src/main/java/tech/noetzold/ecommerce/config/message/RabbitmqConfig.java
@@ -0,0 +1,45 @@
+package tech.noetzold.ecommerce.config.message;
+
+import org.springframework.amqp.core.AcknowledgeMode;
+import org.springframework.amqp.rabbit.config.DirectRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.ConditionalRejectingErrorHandler;
+import org.springframework.amqp.rabbit.listener.DirectMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.FatalExceptionStrategy;
+import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ErrorHandler;
+
+@Configuration
+public class RabbitmqConfig {
+
+    @Bean
+    public RabbitListenerContainerFactory<DirectMessageListenerContainer> rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        DirectRabbitListenerContainerFactory factory = new DirectRabbitListenerContainerFactory();
+
+        factory.setConnectionFactory(connectionFactory);
+        factory.setAcknowledgeMode(AcknowledgeMode.AUTO);
+
+        factory.setPrefetchCount(4);
+        //factory.setGlobalQos(true);
+
+        // Utilizando implementação da ErrorHandler
+        // factory.setErrorHandler(new TratamentoErroHandler());
+
+        // Utilizando implementação da FatalStrategy
+        factory.setErrorHandler(errorHandler());
+
+        return factory;
+    }
+
+    @Bean
+    public FatalExceptionStrategy customErrorStrategy(){
+        return new CustomErrorStrategy();
+    }
+
+    @Bean
+    public ErrorHandler errorHandler() {
+        return new ConditionalRejectingErrorHandler(customErrorStrategy());
+    }
+}
