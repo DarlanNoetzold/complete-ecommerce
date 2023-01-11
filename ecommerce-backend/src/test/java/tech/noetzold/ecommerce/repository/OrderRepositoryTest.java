0c522456054fa0426f91aab9bf92d47dc74fb293
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-11 09:04:17 -0300

    .

---
+++ ecommerce-backend/src/test/java/tech/noetzold/ecommerce/repository/OrderRepositoryTest.java
@@ -0,0 +1,14 @@
+package tech.noetzold.ecommerce.repository;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@DisplayName("Tests for Order Repository")
+@Log4j2
+class OrderRepositoryTest {
+
+}
