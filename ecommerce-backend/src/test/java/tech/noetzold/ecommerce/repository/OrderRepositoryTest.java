888985dff22cb88cff4be5daec9e64dda6320424
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-12 08:45:25 -0300

    Update OrderRepositoryTest.java

--- ecommerce-backend/src/test/java/tech/noetzold/ecommerce/repository/OrderRepositoryTest.java
+++ ecommerce-backend/src/test/java/tech/noetzold/ecommerce/repository/OrderRepositoryTest.java
@@ -1,6 +1,7 @@
 package tech.noetzold.ecommerce.repository;
 
 import lombok.extern.log4j.Log4j2;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +12,6 @@ import tech.noetzold.ecommerce.model.User;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @DisplayName("Tests for Order Repository")
@@ -24,11 +22,32 @@ class OrderRepositoryTest {
     private OrderRepository orderRepository;
 
     @Test
-    @DisplayName("Save the new Order")
+    @DisplayName("Persist the new Order")
     void save_PersistOrder_WhenSuccessful(){
         Order orderToBeSaved = createOrder();
 
         Order orderSaved = this.orderRepository.save(orderToBeSaved);
+
+        Assertions.assertThat(orderSaved).isNotNull();
+        Assertions.assertThat(orderSaved.getId()).isNotNull();
+        Assertions.assertThat(orderSaved.getTotalPrice()).isEqualTo(orderToBeSaved.getTotalPrice());
+
+    }
+
+    @Test
+    @DisplayName("Update the new Order")
+    void save_UpdateOrder_WhenSuccessful(){
+        Order orderToBeSaved = createOrder();
+
+        Order orderSaved = this.orderRepository.save(orderToBeSaved);
+
+        orderSaved.setTotalPrice(15.6);
+
+        Order orderUpdated = this.orderRepository.save(orderSaved);
+        Assertions.assertThat(orderSaved).isNotNull();
+        Assertions.assertThat(orderSaved.getId()).isNotNull();
+        Assertions.assertThat(orderUpdated.getTotalPrice()).isEqualTo(orderSaved.getTotalPrice());
+
     }
 
 
