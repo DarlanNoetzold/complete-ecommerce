43422dfda347f7a89f9757f63486f254d9d76ea8
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-14 19:42:48 -0300

    Update CategoryrepositoryTest.java

--- ecommerce-backend/src/test/java/tech/noetzold/ecommerce/repository/CategoryrepositoryTest.java
+++ ecommerce-backend/src/test/java/tech/noetzold/ecommerce/repository/CategoryrepositoryTest.java
@@ -1,7 +1,88 @@
 package tech.noetzold.ecommerce.repository;
 
+import lombok.extern.log4j.Log4j2;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import tech.noetzold.ecommerce.model.Category;
+import tech.noetzold.ecommerce.util.EcommerceCreator;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class CategoryrepositoryTest {
+@DataJpaTest
+@DisplayName("Tests for Category Repository")
+@Log4j2
+class CategorysRepositoryTest {
+    @Autowired
+    private Categoryrepository categoryRepository;
 
+    @Test
+    @DisplayName("Persist the new Category")
+    void save_PersistOrder_WhenSuccessful(){
+        Category categoryToBeSaved = EcommerceCreator.createCategory();
+
+        Category categorySaved = this.categoryRepository.save(categoryToBeSaved);
+
+        Assertions.assertThat(categorySaved).isNotNull();
+        Assertions.assertThat(categorySaved.getId()).isNotNull();
+        Assertions.assertThat(categorySaved.getCategoryName()).isEqualTo(categoryToBeSaved.getCategoryName());
+
+    }
+
+    @Test
+    @DisplayName("Update the new Category")
+    void save_UpdateOrder_WhenSuccessful(){
+        Category categoryToBeSaved = EcommerceCreator.createCategory();
+
+        Category categorySaved = this.categoryRepository.save(categoryToBeSaved);
+
+        categorySaved.setDescription("aaaaaaaaa");
+
+        Category categoryUpdated = this.categoryRepository.save(categorySaved);
+        Assertions.assertThat(categorySaved).isNotNull();
+        Assertions.assertThat(categorySaved.getId()).isNotNull();
+        Assertions.assertThat(categoryUpdated.getDescription()).isEqualTo(categorySaved.getDescription());
+
+    }
+
+    @Test
+    @DisplayName("Delete the new Category")
+    void save_DeleteOrder_WhenSuccessful(){
+        Category categoryToBeSaved = EcommerceCreator.createCategory();
+
+        Category categorySaved = this.categoryRepository.save(categoryToBeSaved);
+
+        this.categoryRepository.delete(categorySaved);
+
+        Optional<Category> orderOptional = this.categoryRepository.findById(categorySaved.getId());
+
+        Assertions.assertThat(orderOptional).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Find By Id returns a Category when Successful")
+    void findByName_ReturnsListOfAnime_WhenSuccessful(){
+        Category categoryToBeSaved = EcommerceCreator.createCategory();
+
+        Category categorySaved = this.categoryRepository.save(categoryToBeSaved);
+
+        Optional<Category> orderOptional = this.categoryRepository.findById(categorySaved.getId());
+
+        Assertions.assertThat(orderOptional)
+                .isNotEmpty()
+                .contains(categorySaved);
+
+    }
+
+    @Test
+    @DisplayName("Find Category By id and return empty")
+    void findByName_ReturnsEmptyList_WhenAnimeIsNotFound(){
+        Optional<Category> order = this.categoryRepository.findById(5);
+
+        Assertions.assertThat(order).isEmpty();
+    }
 }
