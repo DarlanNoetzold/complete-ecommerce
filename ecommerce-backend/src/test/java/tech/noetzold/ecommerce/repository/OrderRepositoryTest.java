537d11167946d21fa4515e54044f607027fe6225
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-14 18:17:09 -0300

    Update OrderRepositoryTest.java

--- ecommerce-backend/src/test/java/tech/noetzold/ecommerce/repository/OrderRepositoryTest.java
+++ ecommerce-backend/src/test/java/tech/noetzold/ecommerce/repository/OrderRepositoryTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import tech.noetzold.ecommerce.model.*;
-import tech.noetzold.ecommerce.repository.util.EcommerceCreator;
+import tech.noetzold.ecommerce.util.EcommerceCreator;
 
 import java.util.Optional;
 
