959ad799ede8df5dd82eec0d4deaebc244a02374
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-15 20:45:14 -0300

    Update WishListRepositoryTest.java

--- ecommerce-backend/src/test/java/tech/noetzold/ecommerce/repository/WishListRepositoryTest.java
+++ ecommerce-backend/src/test/java/tech/noetzold/ecommerce/repository/WishListRepositoryTest.java
@@ -1,7 +1,81 @@
 package tech.noetzold.ecommerce.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import tech.noetzold.ecommerce.model.User;
+import tech.noetzold.ecommerce.model.WishList;
+import tech.noetzold.ecommerce.util.EcommerceCreator;
+
+import java.util.Optional;
 
 class WishListRepositoryTest {
+    @Autowired
+    private WishListRepository wishListRepository;
+
+    @Test
+    @DisplayName("Persist the new WishList")
+    void save_PersistOrder_WhenSuccessful(){
+        WishList wishListToBeSaved = EcommerceCreator.createWishList();
+
+        WishList wishListSaved = this.wishListRepository.save(wishListToBeSaved);
+
+        Assertions.assertThat(wishListSaved).isNotNull();
+        Assertions.assertThat(wishListSaved.getId()).isNotNull();
+        Assertions.assertThat(wishListSaved.getUser()).isEqualTo(wishListToBeSaved.getUser());
+
+    }
+
+    @Test
+    @DisplayName("Update the new WishList")
+    void save_UpdateOrder_WhenSuccessful(){
+        WishList wishListToBeSaved = EcommerceCreator.createWishList();
+
+        WishList wishListSaved = this.wishListRepository.save(wishListToBeSaved);
+        User wishList = EcommerceCreator.createUser();
+        wishListSaved.setUser(wishList);
+
+        WishList wishListUpdated = this.wishListRepository.save(wishListSaved);
+        Assertions.assertThat(wishListSaved).isNotNull();
+        Assertions.assertThat(wishListSaved.getId()).isNotNull();
+        Assertions.assertThat(wishListUpdated.getUser()).isEqualTo(wishListSaved.getUser());
+
+    }
+
+    @Test
+    @DisplayName("Delete the new WishList")
+    void save_DeleteOrder_WhenSuccessful(){
+        WishList wishListToBeSaved = EcommerceCreator.createWishList();
+
+        WishList wishListSaved = this.wishListRepository.save(wishListToBeSaved);
+
+        this.wishListRepository.delete(wishListSaved);
+
+        Optional<WishList> orderOptional = this.wishListRepository.findById(wishListSaved.getId());
+
+        Assertions.assertThat(orderOptional).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Find By email returns a WishList when Successful")
+    void findByName_ReturnsListOfAnime_WhenSuccessful(){
+        WishList wishListToBeSaved = EcommerceCreator.createWishList();
+
+        WishList wishListSaved = this.wishListRepository.save(wishListToBeSaved);
+
+        Optional<WishList> wishListReturned = this.wishListRepository.findById(wishListSaved.getId());
+
+        Assertions.assertThat(wishListReturned.get()).isEqualTo(wishListSaved);
+
+    }
+
+    @Test
+    @DisplayName("Find WishList By id and return empty")
+    void findByName_ReturnsEmptyList_WhenAnimeIsNotFound(){
+        Optional<WishList> order = this.wishListRepository.findById(5);
+
+        Assertions.assertThat(order).isEmpty();
+    }
 
 }
