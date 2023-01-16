0db1d091980532f356002d03843c1135ce82a393
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-16 08:22:35 -0300

    Create UserControllerTest.java

---
+++ ecommerce-backend/src/test/java/tech/noetzold/ecommerce/controller/UserControllerTest.java
@@ -0,0 +1,11 @@
+package tech.noetzold.ecommerce.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+class UserControllerTest {
+
+}
