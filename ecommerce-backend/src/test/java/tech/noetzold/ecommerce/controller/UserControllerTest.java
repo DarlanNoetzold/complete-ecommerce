deea181dd8a61ecd557ec3e8cc31d6a08f358504
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-17 08:25:29 -0300

    Update UserControllerTest.java

--- ecommerce-backend/src/test/java/tech/noetzold/ecommerce/controller/UserControllerTest.java
+++ ecommerce-backend/src/test/java/tech/noetzold/ecommerce/controller/UserControllerTest.java
@@ -9,17 +9,21 @@ import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tech.noetzold.ecommerce.dto.ResponseDto;
 import tech.noetzold.ecommerce.dto.user.SignInResponseDto;
+import tech.noetzold.ecommerce.enums.ResponseStatus;
 import tech.noetzold.ecommerce.model.AuthenticationToken;
-import tech.noetzold.ecommerce.model.Order;
 import tech.noetzold.ecommerce.model.User;
+import tech.noetzold.ecommerce.repository.UserRepository;
 import tech.noetzold.ecommerce.service.AuthenticationService;
 import tech.noetzold.ecommerce.service.UserService;
 import tech.noetzold.ecommerce.util.EcommerceCreator;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import static tech.noetzold.ecommerce.config.MessageStrings.USER_CREATED;
 
 
 @ExtendWith(SpringExtension.class)
@@ -35,24 +39,51 @@ class UserControllerTest {
 
     @BeforeEach
     void setUp(){
-        User user = EcommerceCreator.createUser();
-        AuthenticationToken token = EcommerceCreator.createAuthenticationToken();
-
         BDDMockito.when(userService.findAllUser())
                 .thenReturn(List.of(EcommerceCreator.createUser()));
 
-        SignInResponseDto response = new SignInResponseDto("success", token.getToken());
+        BDDMockito.when(userService.signIn(ArgumentMatchers.any()))
+                .thenReturn(new SignInResponseDto("success", EcommerceCreator.createAuthenticationToken().getToken()));
 
-        BDDMockito.when(userService.signIn(EcommerceCreator.createSingIn(user.getEmail(),user.getPassword())))
-                .thenReturn(response);
+        BDDMockito.when(userService.signUp(ArgumentMatchers.any()))
+                .thenReturn(new ResponseDto(ResponseStatus.success.toString(), USER_CREATED));
 
         BDDMockito.when(authenticationService.getUser(ArgumentMatchers.anyString()))
                 .thenReturn(EcommerceCreator.createUser());
+
     }
 
     @Test
     @DisplayName("SignIn a user successful")
     void list_SignInUser_WhenSuccessful(){
+        User user = EcommerceCreator.createUser();
 
+        SignInResponseDto sign =  userController.Signin(EcommerceCreator.createSingIn(user.getEmail(),user.getPassword()));
+
+        Assertions.assertThat(sign).isNotNull();
+
+        Assertions.assertThat(sign.getStatus()).isEqualTo("success");
+
+        Assertions.assertThat(sign.getToken()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("SingUp a user successful")
+    void list_SingUp_WhenSuccessful(){
+        ResponseDto signup =  userController.Signup(EcommerceCreator.createSingUp());
+
+        Assertions.assertThat(signup).isNotNull();
+
+        Assertions.assertThat(signup.getStatus()).isEqualTo("success");
+
+        Assertions.assertThat(signup.getMessage()).isEqualTo(USER_CREATED);
+    }
+
+    @Test
+    @DisplayName("Return all a user successful")
+    void list_ReturnAllUser_WhenSuccessful(){
+        List<User> users = userController.findAllUser("");
+
+        Assertions.assertThat(users).isNotEmpty();
     }
 }
