377e8b4a1866b83057dc41717c85caf3f83a5032
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-25 08:20:00 -0300

    Update ProductControllerTest.java

--- ecommerce-backend/src/test/java/tech/noetzold/ecommerce/controller/ProductControllerTest.java
+++ ecommerce-backend/src/test/java/tech/noetzold/ecommerce/controller/ProductControllerTest.java
@@ -12,12 +12,17 @@ import org.mockito.Mock;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tech.noetzold.ecommerce.common.ApiResponse;
 import tech.noetzold.ecommerce.dto.product.ProductDto;
+import tech.noetzold.ecommerce.model.Category;
+import tech.noetzold.ecommerce.model.Product;
 import tech.noetzold.ecommerce.service.CategoryService;
 import tech.noetzold.ecommerce.service.ProductService;
+import tech.noetzold.ecommerce.service.RabbitmqService;
 import tech.noetzold.ecommerce.util.EcommerceCreator;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @ExtendWith(SpringExtension.class)
@@ -32,14 +37,21 @@ class ProductControllerTest {
     @Mock
     private CategoryService categoryService;
 
+    @Mock
+    RabbitmqService rabbitmqService;
+
     @BeforeEach
     void setUp(){
+        Optional<Category> optionalCategory = Optional.of(EcommerceCreator.createCategory());
 
         BDDMockito.when(productService.getProductById(ArgumentMatchers.any()))
                 .thenReturn(EcommerceCreator.createProduct());
 
         BDDMockito.when(productService.listProducts())
                 .thenReturn(List.of(new ProductDto(EcommerceCreator.createProduct())));
+
+        BDDMockito.when(categoryService.readCategory(ArgumentMatchers.anyInt()))
+                .thenReturn(optionalCategory);
 
     }
 
@@ -52,5 +64,30 @@ class ProductControllerTest {
 
         Assertions.assertThat(products.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
+
+    @Test
+    @DisplayName("Add a product when successful")
+    void addProducts_WhenSuccessful(){
+        ResponseEntity<ApiResponse> productSaved = productController.addProduct(new ProductDto(EcommerceCreator.createProduct()));
+
+        Assertions.assertThat(productSaved).isNotNull();
+
+        Assertions.assertThat(productSaved.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+    }
+
+    @Test
+    @DisplayName("Update a product when successful")
+    void updateProducts_WhenSuccessful(){
+        ProductDto product = new ProductDto(EcommerceCreator.createProduct());
+        ResponseEntity<ApiResponse> productSaved = productController.addProduct(product);
+
+        ResponseEntity<ApiResponse> productUpdate = productController.updateProduct(product.getId(), product);
+
+        Assertions.assertThat(productUpdate).isNotNull();
+
+        Assertions.assertThat(productUpdate.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+
 
 }
