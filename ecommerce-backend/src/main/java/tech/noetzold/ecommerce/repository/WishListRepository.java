237b4739925a2b6ae41bb38e1ce7aa6f70271d1b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-26 08:17:11 -0300

    Update WishListRepository.java

--- ecommerce-backend/src/main/java/tech/noetzold/ecommerce/repository/WishListRepository.java
+++ ecommerce-backend/src/main/java/tech/noetzold/ecommerce/repository/WishListRepository.java
@@ -5,12 +5,14 @@ import tech.noetzold.ecommerce.model.WishList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
 public interface WishListRepository extends JpaRepository<WishList, Integer> {
 
     List<WishList> findAllByUserIdOrderByCreatedDateDesc(Integer userId);
-
-
+    @Transactional
+    @Override
+    <S extends WishList> S save(S s);
 }
