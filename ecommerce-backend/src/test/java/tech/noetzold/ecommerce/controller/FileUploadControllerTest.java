dc12d058d858a2c9141d0e866773be9ebb9fac8f
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-27 11:14:46 -0300

    Create FileUploadControllerTest.java

---
+++ ecommerce-backend/src/test/java/tech/noetzold/ecommerce/controller/FileUploadControllerTest.java
@@ -0,0 +1,25 @@
+package tech.noetzold.ecommerce.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileUploadControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void handleFileUpload() {
+    }
+
+    @Test
+    void getListFiles() {
+    }
+
+    @Test
+    void getFile() {
+    }
+}
