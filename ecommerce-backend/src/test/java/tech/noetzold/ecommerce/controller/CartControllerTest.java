27f8ae84c469e4af6b2de8641a509846c5b518b6
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-29 09:51:14 -0300

    Create CartControllerTest.java

---
+++ ecommerce-backend/src/test/java/tech/noetzold/ecommerce/controller/CartControllerTest.java
@@ -0,0 +1,37 @@
+package tech.noetzold.ecommerce.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+class CartControllerTest {
+
+    @InjectMocks
+    private CartController cartController;
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addToCart() {
+    }
+
+    @Test
+    void getCartItems() {
+    }
+
+    @Test
+    void updateCartItem() {
+    }
+
+    @Test
+    void deleteCartItem() {
+    }
+}
