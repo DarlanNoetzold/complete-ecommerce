ef3bb8e046d9f3ee660f754397c62dbdb2984d9b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-29 09:51:16 -0300

    Update FileUploadControllerTest.java

--- ecommerce-backend/src/test/java/tech/noetzold/ecommerce/controller/FileUploadControllerTest.java
+++ ecommerce-backend/src/test/java/tech/noetzold/ecommerce/controller/FileUploadControllerTest.java
@@ -1,12 +1,19 @@
 package tech.noetzold.ecommerce.controller;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tech.noetzold.ecommerce.model.FileInfo;
 
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.List;
+@ExtendWith(SpringExtension.class)
 class FileUploadControllerTest {
 
     @InjectMocks
@@ -18,14 +25,19 @@ class FileUploadControllerTest {
 
     @Test
     void handleFileUpload() {
-        fileUploadController.handleFileUpload()
     }
 
     @Test
     void getListFiles() {
+        ResponseEntity<List<FileInfo>> files = fileUploadController.getListFiles();
+
+        Assertions.assertThat(files).isNotNull();
     }
 
     @Test
     void getFile() {
+        ResponseEntity<Resource> file = fileUploadController.getFile("aaaa");
+
+        Assertions.assertThat(file.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 }
