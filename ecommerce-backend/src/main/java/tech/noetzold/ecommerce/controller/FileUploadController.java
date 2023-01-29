2e6770e687179cfde255904a605f214ab1e881db
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-29 09:51:12 -0300

    Update FileUploadController.java

--- ecommerce-backend/src/main/java/tech/noetzold/ecommerce/controller/FileUploadController.java
+++ ecommerce-backend/src/main/java/tech/noetzold/ecommerce/controller/FileUploadController.java
@@ -7,7 +7,6 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
