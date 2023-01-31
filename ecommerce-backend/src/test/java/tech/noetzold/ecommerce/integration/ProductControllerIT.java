b361e5336b0f14ee48844be52c182c5c00e9ca04
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-31 08:07:10 -0300

    Create ProductControllerIT.java

---
+++ ecommerce-backend/src/test/java/tech/noetzold/ecommerce/integration/ProductControllerIT.java
@@ -0,0 +1,26 @@
+package tech.noetzold.ecommerce.integration;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.annotation.DirtiesContext;
+import tech.noetzold.ecommerce.repository.ProductRepository;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class ProductControllerIT {
+
+    @Autowired
+    @Qualifier(value = "testRestTemplateRoleUser")
+    private TestRestTemplate testRestTemplateRoleUser;
+    @Autowired
+    @Qualifier(value = "testRestTemplateRoleAdmin")
+    private TestRestTemplate testRestTemplateRoleAdmin;
+
+    @Autowired
+    private ProductRepository productRepository;
+}
