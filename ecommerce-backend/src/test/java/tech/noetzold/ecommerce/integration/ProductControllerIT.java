6438a48d8c4ddd1219dfd5d708eccd29f18ebc37
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-02-01 08:16:26 -0300

    Update ProductControllerIT.java

--- ecommerce-backend/src/test/java/tech/noetzold/ecommerce/integration/ProductControllerIT.java
+++ ecommerce-backend/src/test/java/tech/noetzold/ecommerce/integration/ProductControllerIT.java
@@ -1,13 +1,23 @@
 package tech.noetzold.ecommerce.integration;
 
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.test.annotation.DirtiesContext;
+import tech.noetzold.ecommerce.enums.Role;
+import tech.noetzold.ecommerce.model.User;
 import tech.noetzold.ecommerce.repository.ProductRepository;
+import tech.noetzold.ecommerce.repository.UserRepository;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase
@@ -23,4 +33,46 @@ class ProductControllerIT {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+    private static final User USER = User.builder()
+            .password("{bcrypt}$2a$10$hSTIR1LEGbkA6US1B0IJVeoTsHrFKzPwXSeE40SvIFckopmMHoUTm")
+            .firstName("darlan")
+            .email("teste@mail.com")
+            .role(Role.user)
+            .build();
+
+    private static final User ADMIN = User.builder()
+            .password("{bcrypt}$2a$10$hSTIR1LEGbkA6US1B0IJVeoTsHrFKzPwXSeE40SvIFckopmMHoUTm")
+            .firstName("darlan admin")
+            .email("teste@mail.com")
+            .role(Role.admin)
+            .build();
+
+    @TestConfiguration
+    @Lazy
+    static class Config {
+        @Bean(name = "testRestTemplateRoleUser")
+        public TestRestTemplate testRestTemplateRoleUserCreator(@Value("${local.server.port}") int port) {
+            RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder()
+                    .rootUri("http://localhost:"+port)
+                    .basicAuthentication("darlan", "academy");
+            return new TestRestTemplate(restTemplateBuilder);
+        }
+        @Bean(name = "testRestTemplateRoleAdmin")
+        public TestRestTemplate testRestTemplateRoleAdminCreator(@Value("${local.server.port}") int port) {
+            RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder()
+                    .rootUri("http://localhost:"+port)
+                    .basicAuthentication("darlan admin", "academy");
+            return new TestRestTemplate(restTemplateBuilder);
+        }
+    }
+
+
+    @Test
+    @DisplayName("Integration test to list de products")
+    void list_ReturnListOfProducts_WhenSuccessful(){
+
+    }
 }
